120edab703787bcccb28b52ecf4c86688151cbb0
Will McCulloch <willdmcculloch@gmail.com>
2021-07-05 14:16:53 -0700

    setup dev environment

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+//routes
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
